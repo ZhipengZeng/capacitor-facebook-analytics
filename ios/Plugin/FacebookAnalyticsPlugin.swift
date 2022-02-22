679fd81c13221223d90426b4987d20ab91d2c207
kevin zeng <kevin.zeng@ott.ca>
2022-02-22 10:23:35 -0500

    add ios implementation

--- ios/Plugin/FacebookAnalyticsPlugin.swift
+++ ios/Plugin/FacebookAnalyticsPlugin.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Capacitor
+import FBSDKCoreKit
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -7,12 +8,37 @@ import Capacitor
  */
 @objc(FacebookAnalyticsPlugin)
 public class FacebookAnalyticsPlugin: CAPPlugin {
-    private let implementation = FacebookAnalytics()
+//    private let implementation = FacebookAnalytics()
+//
+//    @objc func echo(_ call: CAPPluginCall) {
+//        let value = call.getString("value") ?? ""
+//        call.resolve([
+//            "value": implementation.echo(value)
+//        ])
+//    }
+    
+    @objc func logEvent(_ call: CAPPluginCall) {
+        guard let event = call.getString("event") else {
+            call.reject("Missing event argument")
+            return;
+        }
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
+        print(event)
+
+        if let valueToSum = call.getDouble("valueToSum") {
+            if let params = call.getObject("params") {
+                AppEvents.logEvent(.init(event), valueToSum: valueToSum, parameters: params)
+            } else {
+                AppEvents.logEvent(.init(event), valueToSum: valueToSum)
+            }
+        } else {
+            if let params = call.getObject("params") {
+                AppEvents.logEvent(.init(event), parameters: params)
+            } else {
+                AppEvents.logEvent(.init(event))
+            }
+        }
+        call.resolve()
     }
+
 }
